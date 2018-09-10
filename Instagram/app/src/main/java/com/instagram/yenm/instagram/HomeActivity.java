abadea964f668d196f39e7a3d4ddc4b4e39f53cd
Yen Sheng Miao <miaoyensheng@gmail.com>
2018-09-10 22:43:19 +1000

    instagram bottom menu layout

---
+++ Instagram/app/src/main/java/com/instagram/yenm/instagram/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.instagram.yenm.instagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.instagram.yenm.instagram.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        setupBottomNavigationView();
+    }
+
+//    private void setupBottomNavigationView(){
+//        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+//        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+//        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+//    }
+}
---
+++ Instagram/app/src/main/java/com/instagram/yenm/instagram/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.instagram.yenm.instagram;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "searchActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        
+    }
+
+}
